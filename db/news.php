086634493b1eda1ff3339063618b12867c11cd32
Pupastis <bralic.tina@gmail.com>
2019-01-18 16:27:30 +0100

    Add files via upload

---
+++ db/news.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.8.4
+ */
+
+/**
+ * Database `webprog`
+ */
+
+/* `webprog`.`news` */
+$news = array(
+  array('id' => '1','title' => 'Velebit hiking trail<','description' => '9-day trekking through the most beautiful croatian mountain.','picture' => 'news-1.jpg','date' => '2019-01-14 13:51:20','archive' => 'N'),
+  array('id' => '2','title' => 'Best hiking gear','description' => 'Don’t waste your money on gear that’s no good; I’ve already done that for you! Here’s my trail-tested best hiking gear list for 2018, updated regularly. I only recommend hiking gear that I’ve used over hundreds of miles. I don’t post any paid reviews and don’t waste your time with gear that I try but isn’t up to snuff.
+
+For most gear on this site, I link to REI, and when you click on the link and purchase the item, I receive a very small affiliate commission. I use that commission to pay for website hosting and other costs of running HikingGuy.
+
+So please use the links on this page to buy your gear. Think of it as an easy way to support free hiking guides for everyone.
+
+And don’t forget, use your gear at home first on a simple walk and get used to it before you take it out on the trail','picture' => 'news-2.jpg','date' => '2019-01-14 13:51:20','archive' => 'N'),
+  array('id' => '3','title' => 'Plitvice zimi','description' => 'Polazak autobusom iz Zagreba s parkirališta iza Cibone u 7.30 sati.','picture' => 'news-3.jpg','date' => '2019-01-14 13:51:20','archive' => 'N')
+);
---
+++ db/users.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.8.4
+ */
+
+/**
+ * Database `webprog`
+ */
+
+/* `webprog`.`users` */
+$users = array(
+  array('id' => '17','firstname' => 'pero','lastname' => 'pero','email' => 'pero@pero.com','username' => 'peropero','password' => '$2y$12$4RX1IxyxRn7sP29b9G3ylOsIy4W5gKcfU.qJbYRScBsVZ0rHxLTRW','country' => 'HR','date' => '2019-01-18 16:14:35','archive' => 'Y')
+);
